86fce804735d73a0ea66fa36b18676b6724ddc1b
Sonu-Hansda <sonukumarhansda61@gmail.com>
2024-05-21 15:36:24 +0530

    initial project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Top } from './components'
 import Home from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
      <Home />
