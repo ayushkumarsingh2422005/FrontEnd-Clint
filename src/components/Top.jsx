d7ca89dfcd2eeabd138e03ce19e9db2d2d84c166
丹ⓨㄩک廾 <59462658+ayushkumarsingh2422005@users.noreply.github.com>
2024-05-21 14:40:30 +0530

    updates

---
+++ src/components/Top.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Top() {
+  return (
+    <div className='flex align-middle w-full bg-gray-900 text-gray-200 px-4 py-2 border-b-[1.5px] sticky top-0'>
+      <div className='text-[40px] uppercase'>
+        Affectionary
+      </div>
+    </div>
+  )
+}
