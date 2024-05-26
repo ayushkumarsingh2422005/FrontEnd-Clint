d62f1318ceec796232d13716148277e8314e3032
Sonu-Hansda <sonukumarhansda61@gmail.com>
2024-05-26 17:30:39 +0530

    changed designed for admin and employee only

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import cousineImage from '../assets/cousines.png';
-import adminProfile from '../assets/Admin-Profile.png';
 
 export default function Root() {
     return (
         <>
-            <Link to={'/admin'} className='w-12 fixed right-2 top-2'>
-                <div className='w-12 fixed right-2 top-2 flex flex-col'>
-                    <img src={adminProfile} alt="admin profile" className='rounded-full bg-slate-600'/>
-                </div>
-            </Link>
             <section className='h-screen max-w-screen'>
                 <div className='h-3/4 relative px-6'>
                     <img className='-z-20 h-full w-full absolute inset-0 object-cover' src={cousineImage} alt="Cousine Image" />
@@ -19,10 +13,10 @@ export default function Root() {
                 </div>
                 <div className='px-6 py-4 bg-gray-100 h-1/4'>
                     <h2 className='text-secondary font-semibold text-xl mb-2'>Continue as</h2>
-                    <Link to={'/home'}>
-                        <button className='border p-2 rounded-md hover:shadow-lg transition-shadow ease-linear mr-2 bg-secondary text-white'>Customer</button>
+                    <Link to={'/admin/login'}>
+                        <button className='border py-2 px-4 rounded-md hover:shadow-lg transition-shadow ease-linear mr-2 bg-secondary text-white'>Admin</button>
                     </Link>
-                    <Link to={'/employee'}>
+                    <Link to={'/home'}>
                         <button className='border border-secondary p-2 rounded-md hover:shadow-lg transition-shadow ease-linear'>Employee</button>
                     </Link>
                 </div>
